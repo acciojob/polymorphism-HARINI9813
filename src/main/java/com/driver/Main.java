78f795ca62f7f55f9af464c05f54a79be25b3ce3
HARINI-RAJAN <harinirajan8184@gmail.com>
2024-02-08 01:30:05 +0530

    submitting solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
+class product{
+    public int product(int x, int y)
+    {
+        return 0;
+    }
+    public int product(int x, int y, int z) {
+        return 0;
+    }
+    public double product(double x, double y)
+    {
+        return 0;
+    }
+}
 public class Main {
+  product obj=new product();
+  obj.product(0,0);
+  obj.product(0,0,0);
+  obj.product(1.0,1.0)
 
 }
